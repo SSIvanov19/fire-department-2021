fc688e72d4efd2b2a6750b845d513b388bb27c9e
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2021-04-04 11:55:43 +0300

    Fix bug

--- scripts/Logic Layer/accountManager.js
+++ scripts/Logic Layer/accountManager.js
@@ -159,7 +159,7 @@ if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
     localStorage = new LocalStorage('./scratch');
 }
-
+/*
 //Tests
 let am = new AccountManager(localStorage);
 console.log(am.register("test", "Testov", "testtest@gmail.com", "Testtest"));

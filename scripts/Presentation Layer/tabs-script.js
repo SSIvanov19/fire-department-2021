a970c0c2f129dc0313e67aaa541e6758fc060bc4
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-04-20 10:14:08 +0300

    Fix problem with tabs

--- scripts/Presentation Layer/tabs-script.js
+++ scripts/Presentation Layer/tabs-script.js
@@ -1,11 +1,3 @@
-// mobile menu
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
-});
-
 //tabs
 const tabs = document.querySelectorAll('.tabs li');
 const tabContentBoxes = document.querySelectorAll('#tab-content > div');

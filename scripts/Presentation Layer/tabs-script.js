a1418aa51f65b7b4c7ec806d66c6418540388aee
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-04-20 10:06:31 +0300

    create tabs in fire-safety.html

---
+++ scripts/Presentation Layer/tabs-script.js
@@ -0,0 +1,27 @@
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
+
+//tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === target) {
+                box.classList.remove('is-active');
+            } else {
+                box.classList.add('is-hidden');
+            }
+        });
+    })
+})

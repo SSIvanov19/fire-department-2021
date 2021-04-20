b1e0b900575a93a36f3af94e1b556194a7c4c1df
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2021-04-20 16:58:20 +0300

    Fix problem with tabs-script.js. Fix #50

--- scripts/Presentation Layer/tabs-script.js
+++ scripts/Presentation Layer/tabs-script.js
@@ -4,13 +4,13 @@ const tabContentBoxes = document.querySelectorAll('#tab-content > div');
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
-        tabs.forEach(item => item.classList.remove('is-active'))
+        tabs.forEach(item => item.classList.remove('is-active'));
         tab.classList.add('is-active');
 
         const target = tab.dataset.target;
         tabContentBoxes.forEach(box => {
             if (box.getAttribute('id') === target) {
-                box.classList.remove('is-active');
+                box.classList.remove('is-hidden');
             } else {
                 box.classList.add('is-hidden');
             }

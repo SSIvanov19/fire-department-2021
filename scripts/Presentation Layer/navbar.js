3f36dc278b620986b46fcb318d42351cf1978f4c
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2021-04-24 20:30:30 +0300

    Fix bug

--- scripts/Presentation Layer/navbar.js
+++ scripts/Presentation Layer/navbar.js
@@ -1,6 +1,6 @@
 let am = new AccountManager(localStorage);
 
-if (am.checkForEnterUser()) {
+if (am.checkForEnterUser() == "true") {
   document.getElementById("userLogin").style.display = "inline";
   document.getElementById("userNotLogin").style.display = "none";
 } else {
